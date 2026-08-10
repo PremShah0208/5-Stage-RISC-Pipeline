7a12049d450d82ce0feb91db3629669b25bee3e3
Prem Shah <82882435+PremShah0208@users.noreply.github.com>
2026-08-10 13:18:06 -0700

    Add register file module

---
+++ src/register_file.v
@@ -0,0 +1,23 @@
+module register_file(
+    input clk,
+    input reg_write,
+    input [4:0] rs1,
+    input [4:0] rs2,
+    input [4:0] rd,
+    input [31:0] write_data,
+    output [31:0] read_data1,
+    output [31:0] read_data2
+);
+
+    reg [31:0] regs [0:31];
+
+    assign read_data1 = regs[rs1];
+    assign read_data2 = regs[rs2];
+
+    always @(posedge clk) begin
+        if (reg_write && rd != 0) begin
+            regs[rd] <= write_data;
+        end
+    end
+
+endmodule
